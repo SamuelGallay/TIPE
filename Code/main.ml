86c56a221201a873097178ccf5d694747d957023
Samuel Gallay <samuel.gallay@gmail.com>
2021-05-01 15:49:49 +0200

    Code Rewrite

--- Code/main.ml
+++ Code/main.ml
@@ -1,22 +1,22 @@
 let () =
-  Format.printf "Bienvenue !\n\n" ;
-  Tests.zebra () ;
-  let world =
-    Solver.read_program
-      "
-sous_sujet(alice, groupe1).
-sous_sujet(groupe1, groupe2).
-sous_sujet(bob, groupe2).
+  Format.printf "Bienvenue !\n\n";
+  Prolog.request
+    "
+         sous_sujet(alice, groupe1).
+     sous_sujet(groupe1, groupe2).
+     sous_sujet(bob, groupe2).
 
-sous_objet(fichierA, dossier1).
-sous_objet(fichierB, dossier2).
-sous_objet(dossier1, dossier2).
+     sous_objet(fichierA, dossier1).
+     sous_objet(fichierB, dossier2).
+     sous_objet(dossier1, dossier2).
 
-autorise(groupe1, dossier2).
+     autorise(groupe1, dossier2).
 
-autorise(U, F) :-  sous_objet(F, D), autorise(U, D).
-autorise(U, F) :-  sous_sujet(U, G), autorise(G, F).
-"
-  in
-  let req = Solver.request world in
-  req "autorise(U, F) ?"
+     autorise(U, F) :-  sous_objet(F, D), autorise(U, D).
+     autorise(U, F) :-  sous_sujet(U, G), autorise(G, F)."
+    "autorise(U, F) ?";
+  Tests.zebra ();
+  Tests.test3 ();
+  Tests.test4 ();
+  Tests.test5 ();
+  Tests.test6 ()
